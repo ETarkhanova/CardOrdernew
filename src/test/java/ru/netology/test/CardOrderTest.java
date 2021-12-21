1db2b2c6431e352b9dd3c90abd66d9766a9de29c
Ekaterina Tarkhanova <katenokkt556@gmail.com>
2021-12-21 19:45:23 +0500

    Initial commit

---
+++ src/test/java/ru/netology/test/CardOrderTest.java
@@ -0,0 +1,27 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrderTest {
+    private Selenide div;
+
+    @Test
+    void shouldTest() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Иванов-Петров Федор");
+        form.$("[data-test-id=phone] input").setValue("+79992223334");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+        $("[data-test-id=order-success]").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+}
